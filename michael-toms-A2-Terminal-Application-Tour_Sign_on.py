0ee42c672a5ba597525d9ef2207f823a68dcdac8
Michael Toms <css0119020@coderacademy.edu.au>
2019-07-05 15:58:49 +1000

    michael-toms-A2-Terminal-Application-Tour_Sign_on.py

--- michael-toms-A2-Terminal-Application-Tour_Sign_on.py
+++ michael-toms-A2-Terminal-Application-Tour_Sign_on.py
@@ -28,26 +28,49 @@ def allergy():
         specific_allergy = input("What sort of allergy do you have? ")
         print(f'We see that you have an allergy called: {specific_allergy}, we\'ll make sure that you\'re catered for')
 
-def day():
-    da = input(f'what day of the month were you born on')
-    return da
+def get_day():
+    r = range(1,32)
+    while True:
+        try:
+            day = int(input(f'what day of the month were you born on'))
+        except ValueError:
+            print('Please enter a number from 1-31: ')
+            continue
+        if day not in r:
+            print('Please enter a number from 1-31')
+            continue 
+        return day
 
-def month():
-    mo = input(f'and what number is the month of your birth, please type Month like JAN, FEB etc')
-    return mo
-
-def year():
-    ye = input(f'lastly, What is the year of your birth')
-    return ye
-
+def get_month():
+    r = range(1,13)
+    while True:
+        try:
+            month = int(input(f'and what number is the month of your birth, 1-12?'
+))
+        except ValueError:
+            print('Please enter a number from 1-12: ')
+            continue
+        if month not in r:
+            print('Please enter a number from 1-12')
+            continue
+        return month
+def get_year():
+    r = range(0,3000)
+    while True:
+        try:
+            year = int(input(f'Lastly, what year were you born in'))
+        except ValueError:
+            print('Please enter a valid year:')
+            continue
+        if year not in r:
+            print('Please enter a valid year')
+            continue 
+        return year
 def dob():
-    date = day(),month(),year()
+    date = get_day(),get_month(),get_year()
     return date
-
 name = get_name()
 date = dob()
-
 allergy()
-
 print(name)
-print(date)
+print(f'and because your dob is {date}')
